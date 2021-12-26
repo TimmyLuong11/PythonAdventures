02e510745089df8e98622d070b5764b32699df8b
TimmyLuong <timmyluong11@gmail.com>
2021-12-26 15:47:42 -0600

    First python code

---
+++ Python.py
@@ -0,0 +1,10 @@
+#Printing Hello World
+print("Hello World!")
+
+
+firstName = input("Please enter in your first name:\n")
+lastName = input("Please enter in your last name:\n")
+gpa = float(input("Please enter in your gpa:\n"))
+gpa += 0.5
+
+print(f"Hello {firstName} {lastName}! We have your gpa as {gpa}.")
